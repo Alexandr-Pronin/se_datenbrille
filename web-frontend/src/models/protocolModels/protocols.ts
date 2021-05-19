2a3bbddde4ea7cff867371704a9d46191efecf8f
Alexander Pronin <aleksandr.pronin@htw-dresden.de>
2021-05-19 21:08:40 +0200

    delete

--- web-frontend/src/models/protocolModels/protocols.ts
+++
@@ -1,9 +0,0 @@
-import { Protocol } from "./protocol";
-import { ProtocolTemplate } from "./protocolTemplate";
-import { Label } from "../label";
-
-export class Protocols {
-  completed: Protocol[] = [];
-  templates: ProtocolTemplate[] = [];
-  labels: Label[] = [];
-}
--- web-frontend/src/test/unit-test/testApp.ts
+++
@@ -1,6 +0,0 @@
-import { App } from"../../App"
-
-it('Test App to return Object App', () =>{
-    let app = new App();
-    expect(app).toBeInstanceOf(App);
-});
