22c55cc563d9f01b7dcb82eb333aa6afba0c8a9a
Alexander Pronin <72982357+Alexandr-Pronin@users.noreply.github.com>
2021-05-20 20:37:20 +0200

    Update floatingButton.tsx
    h

--- web-frontend/src/components/floatingButton.tsx
+++ web-frontend/src/components/floatingButton.tsx
@@ -44,7 +44,6 @@ export class FloatingButton extends React.Component<{
             className="action-button-bottom-right"
             onClick={this.props.onclick && this.props.onclick}
           >
-            {this.props.children}
           </Fab>
         </Zoom>
       </div>
